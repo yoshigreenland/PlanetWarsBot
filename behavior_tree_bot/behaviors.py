17d1e04626197d38d49969f9af439505fd6f5964
Josh <landaverdejoshua039@gmail.com>
2026-07-11 22:48:52 -0700

    Bot prioritizes attacking neutrals over enemies, will only attack if neutral ships is less than half of attacker ships, wil only send enough to capture neutrals no extra

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -6,12 +6,15 @@ from planet_wars import issue_order
 
 def attack_weakest_enemy_planet(state):
 
-
+    #list of planets that already being attacked
+    targeted_planets = {fleet.destination_planet for fleet in state.my_fleets()}
+    #limits available targets to only those that are not already being attacked
+    available_targets = [p for p in state.enemy_planets() if p.ID not in targeted_planets]
     # (2) Find my strongest planet.
     strongest_planet = max(state.my_planets(), key=lambda t: t.num_ships, default=None)
 
     # (3) Find the weakest enemy planet.
-    weakest_planet = min(state.enemy_planets(), key=lambda t: t.num_ships, default=None)
+    weakest_planet = min(available_targets, key=lambda t: t.num_ships, default=None)
 
     if not strongest_planet or not weakest_planet:
         # No legal source or destination
@@ -23,16 +26,20 @@ def attack_weakest_enemy_planet(state):
 
 def spread_to_weakest_neutral_planet(state):
 
+    #list of planets that already being attacked
+    targeted_planets = {fleet.destination_planet for fleet in state.my_fleets()}
+    #limits available targets to only those that are not already being attacked
+    available_targets = [p for p in state.neutral_planets() if p.ID not in targeted_planets]
 
     # (2) Find my strongest planet.
     strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
 
     # (3) Find the weakest neutral planet.
-    weakest_planet = min(state.neutral_planets(), key=lambda p: p.num_ships, default=None)
+    weakest_planet = min(available_targets, key=lambda p: p.num_ships, default=None)
 
-    if not strongest_planet or not weakest_planet:
+    if not strongest_planet or not weakest_planet or strongest_planet.num_ships/2 < weakest_planet.num_ships:
         # No legal source or destination
         return False
     else:
-        # (4) Send half the ships from my strongest planet to the weakest enemy planet.
-        return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+        # (4) Send half the ships from my strongest planet to the weakest neutral planet.
+        return issue_order(state, strongest_planet.ID, weakest_planet.ID, weakest_planet.num_ships + 1)
--- behavior_tree_bot/bt_bot.py
+++ behavior_tree_bot/bt_bot.py
@@ -38,7 +38,7 @@ def setup_behavior_tree():
     spread_action = Action(spread_to_weakest_neutral_planet)
     spread_sequence.child_nodes = [neutral_planet_check, spread_action]
 
-    root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
+    root.child_nodes = [spread_sequence, offensive_plan, attack.copy()]
 
     logging.info('\n' + root.tree_to_string())
     return root
